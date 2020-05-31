29d9b2146c1668301b0137c0a2f899567d14a6b1
verababe <107253586+verababe@users.noreply.github.com>
2020-05-31 06:27:16 +0000

    do way to much to describe

--- app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
@@ -2,4 +2,5 @@ package com.koddev.notificationmusicplayer;
 
 import android.content.Context;
 
-import androidx.test.platform.app.Instrumentatio
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.juni
