626f2684e6083517605d765973d25925afeeb8a6
verababe <107253586+verababe@users.noreply.github.com>
2020-06-01 13:21:45 +0000

    trigger a new build

--- app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
@@ -5,4 +5,5 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.jun
+import org.junit.Test;
+import org.junit.runner.RunWi
