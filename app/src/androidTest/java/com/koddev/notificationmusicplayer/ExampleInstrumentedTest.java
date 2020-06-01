fb05e8f72a0272f94918e6fc29f7c5a2bbd08782
verababe <107253586+verababe@users.noreply.github.com>
2020-06-01 16:57:13 +0000

    delete bin

--- app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
@@ -6,4 +6,8 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
-import org.junit.runner.RunWi
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/*
