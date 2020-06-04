b534236a9051595cf0a7d244c7b9909847741aeb
verababe <107253586+verababe@users.noreply.github.com>
2020-06-04 09:36:23 +0000

    More markdown fixes

--- app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/koddev/notificationmusicplayer/ExampleInstrumentedTest.java
@@ -13,4 +13,6 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testi
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWi
