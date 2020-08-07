ee2019729c5f99ae32e9c4f9147b5b469ab880a0
verababe <107253586+verababe@users.noreply.github.com>
2020-08-07 14:15:22 +0000

    updates with new diffs

--- app/src/main/java/com/koddev/notificationmusicplayer/CreateNotification.java
+++ app/src/main/java/com/koddev/notificationmusicplayer/CreateNotification.java
@@ -49,4 +49,4 @@ public class CreateNotification {
             Intent intentPlay = new Intent(context, NotificationActionService.class)
                     .setAction(ACTION_PLAY);
             PendingIntent pendingIntentPlay = PendingIntent.getBroadcast(context, 0,
-              
+                    intentPlay, PendingIntent.FLAG_UPDATE
