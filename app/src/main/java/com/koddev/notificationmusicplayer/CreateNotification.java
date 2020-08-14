d14a1197da4a25a2a5876c587a156c5296dc0805
verababe <107253586+verababe@users.noreply.github.com>
2020-08-14 05:59:01 +0000

    More code cleanup

--- app/src/main/java/com/koddev/notificationmusicplayer/CreateNotification.java
+++ app/src/main/java/com/koddev/notificationmusicplayer/CreateNotification.java
@@ -60,4 +60,4 @@ public class CreateNotification {
                 Intent intentNext = new Intent(context, NotificationActionService.class)
                         .setAction(ACTION_NEXT);
                 pendingIntentNext = PendingIntent.getBroadcast(context, 0,
-                        
+                        intentNext, PendingIntent.FLA
