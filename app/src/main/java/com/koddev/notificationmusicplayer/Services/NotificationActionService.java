60c9497327952acb6af2b34e88b882af1c87b685
verababe <107253586+verababe@users.noreply.github.com>
2020-10-10 14:28:32 +0000

    updated tests

--- app/src/main/java/com/koddev/notificationmusicplayer/Services/NotificationActionService.java
+++ app/src/main/java/com/koddev/notificationmusicplayer/Services/NotificationActionService.java
@@ -4,4 +4,5 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class NotificationAction
+public class NotificationActionService extends BroadcastReceiver {
+  
