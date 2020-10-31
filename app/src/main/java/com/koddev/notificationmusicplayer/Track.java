9cdfb05b4eb20d80e816b963fd9b09fdf7637be5
verababe <107253586+verababe@users.noreply.github.com>
2020-10-31 20:58:25 +0000

    Update tools.md

--- app/src/main/java/com/koddev/notificationmusicplayer/Track.java
+++ app/src/main/java/com/koddev/notificationmusicplayer/Track.java
@@ -11,3 +11,29 @@ public class Track {
         this.title = title;
         this.artist = artist;
         this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
