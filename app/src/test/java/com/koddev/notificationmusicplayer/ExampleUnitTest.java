a47c2872c30796b0b334dcac6c2ed4937c7ea4e4
verababe <107253586+verababe@users.noreply.github.com>
2021-02-03 19:01:53 +0000

    Update android_hydra.txt

--- app/src/test/java/com/koddev/notificationmusicplayer/ExampleUnitTest.java
+++ app/src/test/java/com/koddev/notificationmusicplayer/ExampleUnitTest.java
@@ -5,4 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the develop
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a h
