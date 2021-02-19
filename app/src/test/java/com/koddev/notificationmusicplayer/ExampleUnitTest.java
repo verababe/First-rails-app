9fd41f97f5db26fb4ff6f7c4178033b6800351c6
verababe <107253586+verababe@users.noreply.github.com>
2021-02-19 15:50:19 +0000

    Add error handling.

--- app/src/test/java/com/koddev/notificationmusicplayer/ExampleUnitTest.java
+++ app/src/test/java/com/koddev/notificationmusicplayer/ExampleUnitTest.java
@@ -7,4 +7,6 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/test
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public 
