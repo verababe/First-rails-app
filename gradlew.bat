457d661677ee0597bfb3dfbc09a1ac3a6dc382c7
verababe <107253586+verababe@users.noreply.github.com>
2022-12-02 17:11:44 +0000

    Change routes

--- gradlew.bat
+++ gradlew.bat
@@ -6,4 +6,7 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setloc
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%D
