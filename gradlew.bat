af05ab9f3cee24b92c616d52eb713ced411622b4
verababe <107253586+verababe@users.noreply.github.com>
2023-01-04 12:22:38 +0000

    udf: Check output buffer length when converting name to CS0

--- gradlew.bat
+++ gradlew.bat
@@ -26,4 +26,4 @@ if "%ERRORLEVEL%" == "0" goto init
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please set the JAVA_HOME vari
+echo Please set the JAVA_HOME variable in your environment to 
