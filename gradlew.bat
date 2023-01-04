16f2b7cb0cf0d1e194b124b0565c6da4326b0975
verababe <107253586+verababe@users.noreply.github.com>
2023-01-04 07:28:30 +0000

    Fix compiling

--- gradlew.bat
+++ gradlew.bat
@@ -24,4 +24,6 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Plea
