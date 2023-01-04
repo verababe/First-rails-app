1ffcb1c74dbc46e1b264c7aa31487f008792b059
verababe <107253586+verababe@users.noreply.github.com>
2023-01-04 13:53:48 +0000

    New Commit

--- gradlew.bat
+++ gradlew.bat
@@ -26,4 +26,5 @@ if "%ERRORLEVEL%" == "0" goto init
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please set the JAVA_HOME variable in your environment to 
+echo Please set the JAVA_HOME variable in your environment to match the
+echo locatio
