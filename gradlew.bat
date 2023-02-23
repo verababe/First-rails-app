fd3f941eb8f75d97f03e02b10d2b1c0bdac43c18
verababe <107253586+verababe@users.noreply.github.com>
2023-02-23 21:53:53 +0000

    Procfile for Heroku

--- gradlew.bat
+++ gradlew.bat
@@ -41,4 +41,8 @@ echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of 
+echo location of your Java installation.
+
+goto fail
+
+:
