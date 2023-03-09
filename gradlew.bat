128d0133e5b83cd3f0d7de4d176da76de8b38ee7
verababe <107253586+verababe@users.noreply.github.com>
2023-03-09 16:09:51 +0000

    Create .coveralls.yml

--- gradlew.bat
+++ gradlew.bat
@@ -66,4 +66,4 @@ set CMD_LINE_ARGS=%*
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpa
