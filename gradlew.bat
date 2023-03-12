ea60deeaf9e070cadfcb8871f4f624cc8987cd2a
verababe <107253586+verababe@users.noreply.github.com>
2023-03-12 11:11:23 +0000

    Make compile.

--- gradlew.bat
+++ gradlew.bat
@@ -69,4 +69,4 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
 :end
-@
+@rem End local scope for the vari
