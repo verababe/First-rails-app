60f3f941b9795b381ef4f3e65d56eb0c4ad6663a
verababe <107253586+verababe@users.noreply.github.com>
2023-03-27 07:17:22 +0000

    Translated using Weblate (Galician)

--- gradlew.bat
+++ gradlew.bat
@@ -74,4 +74,5 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c
+rem the _cmd.exe /c_ return code!
+if  not 
