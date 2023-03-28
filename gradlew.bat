f4735769921f096b9e858584bb39f009294ce02e
verababe <107253586+verababe@users.noreply.github.com>
2023-03-28 07:05:30 +0000

    Bump version to 0.8.4

--- gradlew.bat
+++ gradlew.bat
@@ -75,4 +75,4 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not 
+if  not "" == "%GRADLE_EXIT_CONSOLE%" exi
